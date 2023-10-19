e7e801310ed7065558394eb38232b624b44b195c
Roshan Guragain <twistfreaky@gmail.com>
2023-10-19 10:47:23 +0545

    Implemented Redux Store for Products and Integrated JSON-server for Data; Added Product Filtering Capabilities

---
+++ src/features/product/productApi.js
@@ -0,0 +1,27 @@
+export function fetchAllProducts() {
+    return new Promise(async (resolve) =>{
+      
+// TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
+      const response = await fetch('http://localhost:8080/products') 
+      const data = await response.json()
+      resolve({data})
+    }
+    );
+  }
+
+  export function fetchProductsByFilters(filter) {
+    // filter={"category":"smartphone"}
+    let queryString ='';
+    for (let key in filter){
+      queryString +=`${key}=${filter[key]}&`
+    }
+
+    return new Promise(async (resolve) =>{
+      
+// TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
+      const response = await fetch('http://localhost:8080/products?'+queryString) 
+      const data = await response.json()
+      resolve({data})
+    }
+    );
+  }
