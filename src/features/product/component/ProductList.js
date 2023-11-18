1760bd24e9a7ddaca851b9f29374a8b288cf4b1f
Roshan Guragain <twistfreaky@gmail.com>
2023-11-18 14:18:18 +0545

    sorting features checked unchecked improved

--- src/features/product/component/ProductList.js
+++ src/features/product/component/ProductList.js
@@ -238,28 +238,46 @@ export default function ProductList() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const products = useSelector(selectAllProducts);
   const [filter, setFilter] = useState({});
+  const [sort, setSort] = useState({});
   // console.log("Current Filter:", filter);
 
   const handleFilter = (e, section, option) => {
     // e.preventDefault();
-    const newFilter = { ...filter, [section.id]: option.value };
+    const newFilter ={...filter}
+    console.log(e.target.checked)
+    // TODO : on server it will support multiple categories
+    if(e.target.checked){
+      if(newFilter[section.id]){
+        newFilter[section.id].push(option.value)
+      }
+      else{
+        newFilter[section.id]=[option.value]
+      }
+      
+    }
+    else{
+      
+      const index =newFilter[section.id].findIndex(el=>el===option.value)
+        newFilter[section.id].splice(index,1)
+    }
+    console.log(newFilter)
     setFilter(newFilter);
     // console.log("New Filter:", newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
-    console.log(section.id, option.value);
+    // dispatch(fetchProductsByFiltersAsync(newFilter));
+    // console.log(section.id, option.value);
   }; 
 
   const handleSort = (e, option) => {
     // e.preventDefault();
-    const newFilter = { ...filter, _sort: option.sort, _order: option.order };
-    setFilter(newFilter);
-    // console.log("New Filter:", newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
+    const sort = {  _sort: option.sort, _order: option.order };
+     console.log(sort);
+    setSort(sort);
+  
   };
 
   useEffect(() => {
-    dispatch(fetchAllProductsAsync());
-  }, [dispatch]);
+    dispatch(fetchProductsByFiltersAsync(filter,sort));
+  }, [dispatch,filter,sort]); 
 
   return (
     <div>
--- src/features/product/productApi.js
+++ src/features/product/productApi.js
@@ -1,27 +1,37 @@
 export function fetchAllProducts() {
-    return new Promise(async (resolve) =>{
-      
-// TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
-      const response = await fetch('http://localhost:8080/products') 
-      const data = await response.json()
-      resolve({data})
+  return new Promise(async (resolve) => {
+
+    // TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
+    const response = await fetch('http://localhost:8080/products')
+    const data = await response.json()
+    resolve({ data })
+  }
+  );
+}
+
+export function fetchProductsByFilters(filter,sort) {
+  // filter={"category":"smartphone"}
+  // sort = {_sort:"price", _order="desc"}
+  // TODO : ON SERVER WE WILL SUPPORT MULTI VALUES
+  let queryString = '';
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1]
+      queryString += `${key}=${lastCategoryValue}&`
     }
-    );
+
+  }
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
   }
 
-  export function fetchProductsByFilters(filter) {
-    // filter={"category":"smartphone"}
-    let queryString ='';
-    for (let key in filter){
-      queryString +=`${key}=${filter[key]}&`
-    }
+  return new Promise(async (resolve) => {
 
-    return new Promise(async (resolve) =>{
-      
-// TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
-      const response = await fetch('http://localhost:8080/products?'+queryString) 
-      const data = await response.json()
-      resolve({data})
-    }
-    );
+    // TODO: We will not statically set the server URL here; instead, we will dynamically configure it.
+    const response = await fetch('http://localhost:8080/products?' + queryString)
+    const data = await response.json()
+    resolve({ data })
   }
+  );
+}
